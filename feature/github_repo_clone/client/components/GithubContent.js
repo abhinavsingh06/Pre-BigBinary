e9521333419571e37bc7153beabfcf6dc17b1d77
Abhinav <singh.abhinav6996@gmail.com>
2020-02-24 22:21:44 +0530

    add:search functionality

--- feature/github_repo_clone/client/components/GithubContent.js
+++ feature/github_repo_clone/client/components/GithubContent.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './Search';
 import Filter from './Filter';
 import Repos from './Repos';
 
 export default function GithubContent() {
+  const [searchData, setSearchData] = useState('');
   return (
     <>
       <div className='github_content'>
         <div className='github_content_container'>
-          <Search />
+          <Search search={searchData} setSearchData={setSearchData} />
           <Filter />
         </div>
       </div>
       <div className='repo_container'>
-        <Repos />
+        <Repos searchData={searchData} />
       </div>
     </>
   );
--- feature/github_repo_clone/client/components/Repos.js
+++ feature/github_repo_clone/client/components/Repos.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
-export default function Repos() {
+export default function Repos({ searchData }) {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState([]);
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3000/api/v1/')
-      .then((res) => setData(res.data.allRepos));
-  }, []);
+    axios.get('http://localhost:3000/api/v1/').then((res) => {
+      setData(res.data.allRepos);
+      setSearch(
+        res.data.allRepos.filter((data) =>
+          data.name.toLowerCase().includes(searchData.toLowerCase())
+        )
+      );
+    });
+  }, [searchData]);
 
   return (
     <>
       <div>
-        {data.map((item) => {
+        {search.map((item) => {
           return (
             <div className='repo' key={item._id}>
               <h1>{item.name}</h1>
@@ -25,3 +32,7 @@ export default function Repos() {
     </>
   );
 }
+
+Repos.propsTypes = {
+  searchData: PropTypes.string,
+};
